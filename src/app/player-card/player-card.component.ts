54334f8e8a1e773805006d72db2d53cb1281bb62
Bene Sándor <benes@jusoft.hu>
2022-12-22 19:48:49 +0100

    bootstrap + regular/start field round
    * auto deduction of fees (insurances and leases)

--- src/app/player-card/player-card.component.ts
+++ src/app/player-card/player-card.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { CheckboxType, Constants, RoundType } from '../shared/constants';
 import { Player } from '../shared/models/player.model';
-import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
-import { Constants } from '../shared/constants';
 
 @Component({
   selector: 'app-player-card',
@@ -12,6 +12,8 @@ export class PlayerCardComponent implements OnInit {
 
   formGroup: UntypedFormGroup;
   leaseGroup: UntypedFormGroup;
+  RoundType = RoundType;
+  CheckboxType = CheckboxType;
 
   @Input() player: Player;
   @Output() clickHappened = new EventEmitter<Player>();
@@ -90,26 +92,6 @@ export class PlayerCardComponent implements OnInit {
 
   }
 
-
-  autoRepayHouse() {
-    if (this.player.houseRepaymentLeft >= Constants.REPAY_HOUSE) {
-      this.player.actualMoney -= Constants.REPAY_HOUSE;
-      this.player.houseRepaymentLeft -= Constants.REPAY_HOUSE;
-      this.resetControl(this.leaseGroup, 'houseRepay');
-      this.emitPlayer();
-    }
-  }
-
-  autoRepayCar() {
-    if (this.player.carRepaymentLeft >= Constants.REPAY_CAR) {
-      this.player.actualMoney -= Constants.REPAY_CAR;
-      this.player.carRepaymentLeft -= Constants.REPAY_CAR;
-      this.resetControl(this.leaseGroup, 'carRepay');
-      this.emitPlayer();
-    }
-  }
-
-
   private resetControl(group: UntypedFormGroup, controlName: string) {
     group.controls[controlName].reset();
     group.controls[controlName].markAsPristine();
@@ -120,6 +102,72 @@ export class PlayerCardComponent implements OnInit {
 
   private emitPlayer() {
     this.clickHappened.emit(this.player);
+  }
+
+  onRound(round: RoundType): void {
+    this.player.actualMoney += round === RoundType.REGULAR ? Constants.REGULAR_ROUND_MONEY : Constants.START_ROUND_MONEY;
+    this.deductMorgageAndCarInterest();
+  }
+
+  private deductMorgageAndCarInterest(): void {
+    if (this.player.hasMorgage && this.player.houseRepaymentLeft > 0) {
+      const decutionAmount = this.player.houseRepaymentLeft >= Constants.REPAY_HOUSE ? Constants.REPAY_HOUSE : this.player.houseRepaymentLeft;
+      this.player.actualMoney -= decutionAmount;
+      this.player.houseRepaymentLeft -= decutionAmount;
+    }
+
+    if (this.player.hasCarLeased && this.player.carRepaymentLeft > 0) {
+      const deductionCarAmount = this.player.carRepaymentLeft >= Constants.REPAY_CAR ? Constants.REPAY_CAR : this.player.carRepaymentLeft;
+      this.player.actualMoney -= deductionCarAmount;
+      this.player.carRepaymentLeft-= deductionCarAmount;
+    }
+    this.emitPlayer();
+  }
+
+  onCheckboxChange(event, insuranceType: CheckboxType): void {
+    let deductionAmount = 0;
+    switch (insuranceType) {
+      case CheckboxType.RETIREMENT:
+        if (!this.player.insurances.retirementInsurance) {
+          deductionAmount =  Constants.RETIREMENT_INSURANCE;
+          this.player.insurances.retirementInsurance  =event.checked;
+        }
+        break;
+      case CheckboxType.HOME:
+        if (!this.player.insurances.homeInsurance) {
+            deductionAmount = Constants.HOUSE_INSURANCE;
+            this.player.insurances.homeInsurance = event.checked;
+          }
+        break;
+      case CheckboxType.CHILD:
+        if (!this.player.insurances.childInsurance) {
+          deductionAmount = Constants.CHILD_INSURANCE;
+          this.player.insurances.childInsurance  =event.checked;
+
+        }
+        break;
+      case CheckboxType.CASCO:
+        if (!this.player.insurances.casco) {
+          deductionAmount = Constants.CASCO_PRICE;
+          this.player.insurances.casco  =event.checked;
+
+        }
+        break;
+      case CheckboxType.CAR_LEASE:
+        if (!this.player.hasCarLeased) {
+          deductionAmount = Constants.CAR_LEASE_START_PRICE;
+          this.player.hasCarLeased = event.checked;
+        }
+        break;
+      case CheckboxType.MORGAGE:
+        if (!this.player.hasMorgage) {
+          deductionAmount = Constants.HOUSE_MORGAGE_START_PRICE;
+          this.player.hasMorgage = event.checked;
+        }
+        break;
+    }
+    this.player.actualMoney-= deductionAmount;
+    this.emitPlayer();
   }
 
 }
